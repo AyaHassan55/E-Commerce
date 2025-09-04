a74a4c683988f2b9ecda2c579888c8a9e365e0ad
AyaAly9 <ayaaly929@gmail.com>
2025-09-04 16:52:33 +0300

    add to cart

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const container = document.getElementById("products-container");
 const pagination = document.getElementById("pagination");
 let productsData = [];
+let cart = [];
+const cartCount = document.getElementById("cart-count");
+
 
 // Function to render products
 function displayProducts(products) {
@@ -10,14 +13,27 @@ function displayProducts(products) {
         const col = document.createElement("div");
         col.className = "col-6 col-md-6 col-lg-3 mb-4"; 
         col.innerHTML = `
-            <div class="card product-card">
-                <img src="${product.image}" class="card-img-top" alt="${product.name}">
-                <div class="card-body text-center">
-                    <h6 class="card-title">${product.name}</h6>
-                    <p>₹ ${product.price}</p>
-                </div>
-            </div>
-        `;
+    <div class="card product-card position-relative">
+        <img src="${product.image}" class="card-img-top" alt="${product.name}">
+        <div class="card-body text-center">
+            <h6 class="card-title">${product.name}</h6>
+            <p>₹ ${product.price}</p>
+        </div>
+        <div class="add-to-cart position-absolute top-0 end-0 m-2 p-2 bg-dark text-white rounded-circle" title="Add to Cart" style="cursor:pointer;">
+            <i class="fa-solid fa-cart-plus"></i>
+        </div>
+    </div>
+`;
+
+        
+        // زرار إضافة للسلة
+        col.querySelector(".add-to-cart").addEventListener("click", (e) => {
+             e.stopPropagation(); // عشان ميعملش مشاكل مع الكارت
+                addToCart(product);
+        });
+
+    
+        // 
         container.appendChild(col);
     });
 }
@@ -41,6 +57,8 @@ fetch("data/products.json")
                 <a class="page-link" href="#">&gt;</a>
             </li>
         `;
+
+        // 
     })
     .catch(error => console.error("Error loading products:", error));
 
@@ -72,3 +90,25 @@ document.querySelectorAll(".filter-checkbox").forEach(checkbox => {
         displayProducts(filtered);
     });
 });
+
+// ---------------- Add to Cart ----------------
+
+function addToCart(product) {
+    // لو المنتج موجود نزود الكمية
+    let existing = cart.find(item => item.id === product.id);
+    if (existing) {
+        existing.qty += 1;
+    } else {
+        cart.push({ ...product, qty: 1 });
+    } 
+
+    updateCartCount();
+}
+
+// ---------------- Render Cart ----------------
+function updateCartCount() {
+    let totalQty = cart.reduce((sum, item) => sum + item.qty, 0);
+    cartCount.textContent = totalQty;
+}
+
+
